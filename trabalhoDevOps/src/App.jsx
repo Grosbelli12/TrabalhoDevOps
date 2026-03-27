e4616db502f683b01869499a75386adf1ffc1b1f
grosbelli12 <gustavogrosbelli11@gmail.com>
2026-03-26 21:23:20 -0300

    initial commit

---
+++ trabalhoDevOps/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Gerador de número aleatório</h1>
+      <h2>Selecione o intervalo que deseja gerar o número aleatório</h2>
+    </>
+  );
+}
+
+export default App;
